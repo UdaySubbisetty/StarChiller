00dbc3ae44f978ddad61ea27d4b954eb933363f5
Udaya bhaskar Subbisetty <30430465+UdaySubbisetty@users.noreply.github.com>
2025-04-11 19:28:43 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,27 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <p>Sample</p>
+       <h1>Sample</h1>
     <App />
   </StrictMode>,
 )
